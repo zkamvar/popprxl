f89f7bdf4d4932efc6fd6cfe62cbe262d56056bc
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-05-25 11:52:55 -0700

    add package documentation.

---
+++ R/popprxl-package.r
@@ -0,0 +1,13 @@
+#' popprxl.
+#'
+#' This package has one function: \code{\link{read.genalexcel}}.
+#'
+#' @section Citation:
+#' To cite this package, please use the citation for the poppr package. You can
+#' find it by typing \code{citation(package = "poppr")}.
+#'
+#'
+#' @name popprxl
+#' @author Zhian N. Kamvar and Niklaus J. Grünwald
+#' @docType package
+NULL
